a161895832b742632824c79b0bf740dc6995d998
Yanet Geleta <yanetgele@gmail.com>
2026-03-26 23:36:27 +0300

    finished shows page

--- client/src/pages/Shows.jsx
+++ client/src/pages/Shows.jsx
@@ -8,6 +8,7 @@ import { ClipLoader } from "react-spinners";
 import { useState } from "react";
 import Pagination from "@mui/material/Pagination";
 import FilmItem from "../components/FilmItem";
+import PaginationItem from "@mui/material/PaginationItem";
 // This is a dedicated page for shows
 const Shows = () => {
   // const currentYear = new Date().getFullYear();
@@ -72,61 +73,105 @@ const Shows = () => {
   }, [genreID, sortBy, page]);
 
   return (
-    <div>
-      {/* <Navbar /> */}
-      <h2>Filter & Sort</h2>
-      <FilterAndSort
-        onSortChange={(sortByValue) => {
-          setSortBy(sortByValue);
-        }}
-        currentSortValue={sortBy}
-        filmType="tv"
-      />
-      {/* Genres not working: war and politics, action and adventure, sci-fi and fantasy */}
-      <h2>Genres</h2>
-      <GenresFilter
-        onGenreChange={(genreValue) => {
-          setGenre(genreValue);
-        }}
-        currentGenreValue={genre}
-        filmType="tv"
-      />
-      <Button>Reset</Button>
-      {loading ? (
-        <ClipLoader
-          loading={loading}
-          data-testid="loader"
-          aria-label="Loading shows spinner"
-        />
-      ) : (
-        <div>
-          <h1>Browse Tv Shows</h1>
-          <div className="grid grid-cols-5 gap-4 p-4">
-            {showData &&
-              showData.results.map((show) => (
-                /* 2. THE ITEM: Each child automatically fills one grid cell */
-                <FilmItem
-                  basePosterPath={basePosterPath}
-                  smallBannerWidth={smallBannerWidth}
-                  film={show}
-                  filmType="tv"
-                />
-              ))}
+    <section className="mt-20 flex gap-8 w-full max-w-7xl md:mx-20">
+      <aside className="hidden w-64 flex-shrink-0 lg:block">
+        <div className="sticky top-24 flex flex-col gap-6">
+          <h2 className="text-xl font-semibold ml-2 mb-[-.5rem]">
+            Filter & Sort
+          </h2>
+          <div className="flex flex-col gap-2 rounded-xl border border-slate-200 bg-[#222a3d]/60 p-4 dark:border-slate-800 dark:bg-slate-900">
+            <FilterAndSort
+              onSortChange={(sortByValue) => {
+                setSortBy(sortByValue);
+              }}
+              currentSortValue={sortBy}
+              filmType="tv"
+            />
+            {/* Genres not working: war and politics, action and adventure, sci-fi and fantasy */}
+            <h2>Genres</h2>
+            <GenresFilter
+              onGenreChange={(genreValue) => {
+                setGenre(genreValue);
+              }}
+              currentGenreValue={genre}
+              filmType="tv"
+            />
+            <button
+              onClick={() => {
+                setSortBy(null);
+                setGenre(null);
+              }}
+            >
+              Reset
+            </button>
           </div>
         </div>
-      )}
-      <Pagination
-        onChange={(e, value) => {
-          setPage(value);
-        }}
-        page={page}
-        count={10}
-        color="primary"
-      />
+      </aside>
+      <div className="flex flex-1 flex-col gap-6">
+        {loading ? (
+          <div className="flex flex-grow items-center justify-center w-full min-h-[60vh] mb-8">
+            <ClipLoader
+              loading={loading}
+              data-testid="loader"
+              aria-label="Loading shows spinner"
+              color="white"
+            />
+          </div>
+        ) : (
+          <div className="relative">
+            <h1 className="text-4xl font-black leading-tight tracking-[-0.033em] my-5">
+              Browse Tv Shows
+            </h1>
+            <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-6 gap-y-10">
+              {showData &&
+                showData.results.map((show) => (
+                  /* 2. THE ITEM: Each child automatically fills one grid cell */
+                  <FilmItem
+                    basePosterPath={basePosterPath}
+                    smallBannerWidth={smallBannerWidth}
+                    film={show}
+                    filmType="tv"
+                    key={show.id}
+                  />
+                ))}
+            </div>
+          </div>
+        )}
+        <div className="flex justify-center items-center pt-8">
+          <Pagination
+            onChange={(e, value) => {
+              setPage(value);
+            }}
+            page={page}
+            count={10}
+            color="primary"
+            renderItem={(item) => (
+              <PaginationItem
+                {...item}
+                sx={{
+                  "&.Mui-selected": {
+                    backgroundColor: "#222a3d",
+                    color: "white",
+                    "&:hover": {
+                      backgroundColor: "#31394d",
+                    },
+                  },
+                  "&:not(.Mui-selected)": {
+                    color: "white",
+                  },
+                  "&:hover": {
+                    backgroundColor: "#262348",
+                  },
+                }}
+              />
+            )}
+          />
+        </div>
+      </div>
 
       {/* The film cards obviously will be looped through */}
       {/* We need to add a navigation for next pages, a numbered one */}
-    </div>
+    </section>
   );
 };
 export default Shows;
